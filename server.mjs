356dc352e3909905690405649c324d03c6e35f67
lmfmay <176079985+lmfmay@users.noreply.github.com>
2024-10-09 18:51:14 -0700

    Part 1 completed

--- server.mjs
+++ server.mjs
@@ -10,15 +10,36 @@ app.use(express.static('./styles'));
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
+//extract form data
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     const data = {
-      title: 'Home Page',
-      message: 'Here is your user profile!',
-      user: { name: 'John Doe' }
+      title: 'Login Page',
+      message: 'Please type your name and click the Login button to login'
     };
-    
     res.render('index', data);
   });
+
+//route parameter that modifies what is rendered to the client.
+app.post('/user', (req, res) => {
+    const userName = req.body.name;
+    const data = {
+      title: 'User Page',
+      message: 'Here is your user profile!',
+      user: `${userName}`
+    };    
+    res.render('user', data);
+  });
+
+app.get('/info', (req, res) => {
+const data = {
+    title: 'Info Page',
+    message: 'Here is some information about what you can do!'
+};
+
+res.render('info', data);
+});
 
 //Listen for server
 app.listen(PORT, ()=>{
